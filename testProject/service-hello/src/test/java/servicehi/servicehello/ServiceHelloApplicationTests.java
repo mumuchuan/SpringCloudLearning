d536817d115f031f0d56876070d9eae4aa6c6144
huabin <1351195777@qq.com>
2020-07-07 17:34:38 +0800

    HystrixTurbine【监控多个服务】

---
+++ testProject/service-hello/src/test/java/servicehi/servicehello/ServiceHelloApplicationTests.java
@@ -0,0 +1,13 @@
+//package servicehi.servicehello;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceHelloApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
---
+++ testProject/service-turbine/src/test/java/com/springcloudtest/serviceturbine/ServiceTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.springcloudtest.serviceturbine;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ServiceTurbineApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
